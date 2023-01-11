f134e456d9bbafd7101fcaf16c259c6a60e00504
Dmitry <jump.aroundtheworld@mail.ru>
2023-01-12 00:29:52 +0300

    add brain-gcd, update brain-even

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/rizhik356/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/rizhik356/frontend-project-44/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/2bfba918a065448242d2/maintainability)](https://codeclimate.com/github/rizhik356/frontend-project-44/maintainability)
-
+#### brain-even
 <a href="https://asciinema.org/a/CjvLqEz7NLIvV8Fmbks5kYDpL" target="_blank"><img src="https://asciinema.org/a/CjvLqEz7NLIvV8Fmbks5kYDpL.svg" /></a>
+
+#### brain-calc
 <a href="https://asciinema.org/a/VPjyl2Xoptuggif7E3fQUCi91" target="_blank"><img src="https://asciinema.org/a/VPjyl2Xoptuggif7E3fQUCi91.svg" /></a>
+
+#### brain-gcd
+<a href="https://asciinema.org/a/5mVQIEy3mwcxbz99rEYHieELj" target="_blank"><img src="https://asciinema.org/a/5mVQIEy3mwcxbz99rEYHieELj.svg" /></a>
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,27 +1,13 @@
 #!/usr/bin/env node
-
-import readlineSync from 'readline-sync';
+import gameLogic from '../src/index.js';
 
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const gameTask = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isNumberEven = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  for (let i = 0; i <= 2; i += 1) {
-    const number = getRandomInt(1, 100);
-    const answer = ((number % 2 === 0) ? 'yes' : 'no');
-    console.log(`Question: ${number}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === answer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${userName}`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
+const getStartGame = () => {
+  const question = getRandomInt(1, 100);
+  const answer = ((question % 2 === 0) ? 'yes' : 'no');
+  return [question, answer];
 };
-isNumberEven();
+
+gameLogic(gameTask, getStartGame);
