a2dcf536e9a454c3b345bf06ca852e8580e64e37
Dmitry <jump.aroundtheworld@mail.ru>
2023-01-13 21:48:01 +0300

    add brain-prime.js

---
+++ bin/brain-prime.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import gameLogic from '../src/index.js';
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+
+const getStartGame = () => {
+  const question = getRandomInt(1, 50);
+  let answer = 'no';
+  for (let i = 2; i < question; i += 1) {
+    if (question % i === 0) {
+      answer = 'no';
+      break;
+    } else {
+      answer = 'yes';
+    }
+  }
+  return [question, answer];
+};
+
+gameLogic(gameTask, getStartGame);
